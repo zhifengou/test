832e1434075a430a331ab6d76486c162f89b1c31
zhifeng <zhifengou@hotmail.com>
2019-03-11 18:06:41 -0400

    Updates

---
+++ precache-manifest.711f0c23f9ff8c7a30c0d45820e09933.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "73a5b39303336ccd65fa",
+    "url": "/test/static/css/main.0b69e22f.chunk.css"
+  },
+  {
+    "revision": "73a5b39303336ccd65fa",
+    "url": "/test/static/js/main.f4a1219d.chunk.js"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  },
+  {
+    "revision": "37633691b9724b784dbc",
+    "url": "/test/static/js/2.ff439d2d.chunk.js"
+  },
+  {
+    "revision": "f11bfab1cb9a36c2d46c5dc681583793",
+    "url": "/test/index.html"
+  }
+];
